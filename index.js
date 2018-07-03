87ef3804d3eff4a6cc476168591b71e79a87fbd9
duza <duza@tut.by>
2018-07-03 15:32:43 +0300

    Add babel-preset-2015 plugin
    Now we can use syntax sugar with class ^)

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 document.write("hello, webpack! I'm watching for changes");
 const dress = () => {
-  document.write('.<br> I like your dress');
+  document.write('.<br> I like your dress.<br>');
 }
 dress();
+
+class Car {
+
+  manufacturer(car) {
+    document.write(`I have a ${car}`);
+  }
+}
+
+const bmw = new Car;
+bmw.manufacturer('bmw');
