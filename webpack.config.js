7e0dcc0f0758647b41a58b41a8b787ecc7c83bc1
duza <duza@tut.by>
2018-07-05 00:06:35 +0300

    Add uglify plugin only for production

--- webpack.config.js
+++ webpack.config.js
@@ -31,10 +31,6 @@ let config = {
   ],
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({
-        test: /\.js($|\?)/i,
-        sourceMap: true
-      })
     ],
   },
   devServer: {
@@ -47,4 +43,13 @@ let config = {
 }
 
 module.exports = config;
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.optimization.minimizer.push(
+    new UglifyJsPlugin({
+      test: /\.js($|\?)/i,
+      // sourceMap: true
+    })
+  );
+}
 
